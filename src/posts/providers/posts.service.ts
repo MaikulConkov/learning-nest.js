b06a26ce730b796988d617f78baf7d4d133043f9
Maikul <mcjaiyo01@gmail.com>
2024-10-01 12:51:21 +0300

    post update error handling

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -1,4 +1,9 @@
-import { Body, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Injectable,
+  RequestTimeoutException,
+} from '@nestjs/common';
 import { title } from 'process';
 import { UsersService } from 'src/users/providers/users.service';
 import { CreatePostDto } from '../dtos/create-post.dto';
@@ -56,25 +61,75 @@ export class PostsService {
     return { deleted: true, id };
   }
 
-  public async update(@Body() patchPostsDto: PatchPostDto) {
-    //Find the tags
-    let tags = await this.tagsService.findMultibleTags(patchPostsDto.tags);
+  public async update(patchPostDto: PatchPostDto) {
+    let tags = undefined;
+    let post = undefined;
 
-    //Find the post
-    let post = await this.postsRepository.findOneBy({ id: patchPostsDto.id });
-    //Update the propertied of the post
-    post.title = patchPostsDto.title ?? post.title;
-    post.content = patchPostsDto.content ?? post.content;
-    post.status = patchPostsDto.status ?? post.status;
-    post.postType = patchPostsDto.postType ?? post.postType;
-    post.slug = patchPostsDto.slug ?? post.slug;
+    // Find the Tags
+    try {
+      tags = await this.tagsService.findMultibleTags(patchPostDto.tags);
+    } catch (error) {
+      throw new RequestTimeoutException(
+        'Unable to process your request at the moment please try later',
+        {
+          description: 'Error connecting to the database',
+        },
+      );
+    }
+
+    /**
+     * If tags were not found
+     * Need to be equal number of tags
+     */
+    if (!tags || tags.length !== patchPostDto.tags.length) {
+      throw new BadRequestException(
+        'Please check your tag Ids and ensure they are correct',
+      );
+    }
+
+    // Find the Post
+    try {
+      // Returns null if the post does not exist
+      post = await this.postsRepository.findOneBy({
+        id: patchPostDto.id,
+      });
+    } catch (error) {
+      throw new RequestTimeoutException(
+        'Unable to process your request at the moment please try later',
+        {
+          description: 'Error connecting to the database',
+        },
+      );
+    }
+
+    if (!post) {
+      throw new BadRequestException('The post Id does not exist');
+    }
+
+    // Update the properties
+    post.title = patchPostDto.title ?? post.title;
+    post.content = patchPostDto.content ?? post.content;
+    post.status = patchPostDto.status ?? post.status;
+    post.postType = patchPostDto.postType ?? post.postType;
+    post.slug = patchPostDto.slug ?? post.slug;
     post.featuredImageUrl =
-      patchPostsDto.featuredImageUrl ?? post.featuredImageUrl;
-    post.publishOn = patchPostsDto.publishOn ?? post.publishOn;
-    //Assign the new tags
+      patchPostDto.featuredImageUrl ?? post.featuredImageUrl;
+    post.publishOn = patchPostDto.publishOn ?? post.publishOn;
+
+    // Assign the new tags
     post.tags = tags;
 
-    //Save the post and return it
-    return await this.postsRepository.save(post);
+    // Save the post and return
+    try {
+      await this.postsRepository.save(post);
+    } catch (error) {
+      throw new RequestTimeoutException(
+        'Unable to process your request at the moment please try later',
+        {
+          description: 'Error connecting to the database',
+        },
+      );
+    }
+    return post;
   }
 }
